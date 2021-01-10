cc780e3ddfe545aeda4b43c3805f64f8a26515a9
Ian Kottman <ikottman@users.noreply.github.com>
2021-01-09 19:48:10 -0600

    styling

---
+++ src/Counts.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import dayjs from 'dayjs';
+
+  export let date;
+
+  const today = dayjs();
+  const years = Math.abs(date.diff(today, 'year'));
+  const months = today.month() + 1;
+  const days = today.date();
+</script>
+
+<div class="grid grid-cols-3">
+  <div class="flex justify-center font-bold border-r-4">Years</div>
+  <div class="flex justify-center font-bold border-r-4">Months</div>
+  <div class="flex justify-center font-bold">Days</div>
+  <div class="flex justify-center border-r-4">{years}</div>
+  <div class="flex justify-center border-r-4">{months}</div>
+  <div class="flex justify-center">{days}</div>
+</div>
--- src/Event.svelte
+++ src/Event.svelte
@@ -1,20 +1,15 @@
 <script lang="ts">
   import dayjs from 'dayjs';
+  import Counts from './Counts.svelte';
+
   export let event;
   export let date;
   date = dayjs(date);
   const dateDisplay = date.format("MMMM D, YYYY")
 </script>
 
-<div class="shadow-md p-4">
+<div class="p-4 bg-gray-100">
   <h3 class="flex justify-center text-2xl font-bold">{event}</h3>
   <div class="flex justify-center">{dateDisplay}</div>
-  <div class="grid grid-cols-3 gap-x-4 justify-center">
-    <div class="flex justify-center font-bold">Years</div>
-    <div class="flex justify-center font-bold">Months</div>
-    <div class="flex justify-center font-bold">Days</div>
-    <div class="flex justify-center">10</div>
-    <div class="flex justify-center">12</div>
-    <div class="flex justify-center">13</div>
-  </div>
+  <Counts date={date} />
 </div>
