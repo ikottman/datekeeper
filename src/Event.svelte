ff0dd4c42375de571fa97043e6deb4722b90d2a2
Ian Kottman <ikottman@users.noreply.github.com>
2021-01-09 18:56:12 -0600

    layout

---
+++ src/Event.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import dayjs from 'dayjs';
+  export let event;
+  export let date;
+  date = dayjs(date);
+  const dateDisplay = date.format("MMMM D, YYYY")
+</script>
+
+<div class="shadow-md p-4">
+  <h3 class="flex justify-center text-2xl font-bold">{event}</h3>
+  <div class="flex justify-center">{dateDisplay}</div>
+  <div class="grid grid-cols-3 gap-x-4 justify-center">
+    <div class="flex justify-center font-bold">Years</div>
+    <div class="flex justify-center font-bold">Months</div>
+    <div class="flex justify-center font-bold">Days</div>
+    <div class="flex justify-center">10</div>
+    <div class="flex justify-center">12</div>
+    <div class="flex justify-center">13</div>
+  </div>
+</div>
--- src/Events.svelte
+++ src/Events.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
+  import Event from "./Event.svelte";
   import { getEvents } from "./api";
   const events = getEvents();
 </script>
 
-
-{#await events}
-<div class="flex justify-center m-3">
-  Fetching events
+<div class="grid grid-cols-3 gap-4 m-4">
+  {#await events}
+    <div>
+      Fetching events
+    </div>
+  {:then event}
+    {#each event as {event, date}}
+      <div class="col-start-2">
+        <Event event={event} date={date} />
+      </div>
+    {/each}
+  {/await}
 </div>
-{:then event}
-  {#each event as {event, date}}
-    <div>{event}: {date}</div>
-  {/each}
-{/await}
