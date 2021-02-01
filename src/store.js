8abb0b7276247d715cc065cf2d62de831aaf7181
Ian Kottman <ikottman@users.noreply.github.com>
2021-01-31 20:58:04 -0600

    keep user in localstorage

--- src/store.js
+++ src/store.js
@@ -1,5 +1,14 @@
 import { writable } from 'svelte/store';
 
 // User authentication
-export const user = writable({});
+let storedUser = localStorage.getItem('dateKeeperUser');
+export const user = writable(storedUser ? JSON.parse(storedUser) : null);
+user.subscribe((value) => {
+  if (value) {
+    localStorage.setItem('dateKeeperUser', JSON.stringify(value));
+  } else {
+    localStorage.removeItem('dateKeeperUser');
+  }
+});
+
 export const authenticated = writable(false);
