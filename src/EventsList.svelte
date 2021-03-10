19eaa8cabd4cdc76b68db1b240d190f87ab196f2
Ian Kottman <ikottman@users.noreply.github.com>
2021-03-09 23:40:05 -0600

    fix sorting

--- src/EventsList.svelte
+++ src/EventsList.svelte
@@ -2,9 +2,9 @@
   import Event from "./Event.svelte";
   import { events } from "./store";
 
-  $events.sort((a, b) => a.date.localeCompare(b.date))
+  $:sortedEvents = $events.sort((a, b) => a.date.localeCompare(b.date))
 </script>
-{#each $events as { name, date }}
+{#each sortedEvents as { name, date }}
   <div class="col-start-2 mb-4">
     <Event name={name} date={date} />
   </div>
