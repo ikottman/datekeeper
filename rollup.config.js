8954985d928fce7bf791105f70a5947e5631cb1d
Ian Kottman <ikottman@users.noreply.github.com>
2021-03-13 15:34:30 -0600

    update tailwind and a bunch of stuff to get better grid stylings

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import livereload from "rollup-plugin-livereload"
 import { terser } from "rollup-plugin-terser"
 import sveltePreprocess from "svelte-preprocess"
 import typescript from "@rollup/plugin-typescript"
+import css from 'rollup-plugin-css-only'
 
 const production = !process.env.ROLLUP_WATCH
 function serve() {
@@ -39,17 +40,15 @@ export default {
     },
     plugins: [
         svelte({
-            // enable run-time checks when not in production
-            dev: !production,
-            // we'll extract any component CSS out into
-            // a separate file - better for performance
-            css: (css) => {
-                css.write("public/build/bundle.css")
+            compilerOptions: {
+              dev: !production,
             },
-            preprocess: sveltePreprocess({
-                postcss: true,
-            }),
+            preprocess: sveltePreprocess({ postcss: true }),
         }),
+
+        // we'll extract any component CSS out into
+        // a separate file - better for performance
+        css({ output: 'bundle.css' }),
 
         // If you have external dependencies installed from
         // npm, you'll most likely need these plugins. In
